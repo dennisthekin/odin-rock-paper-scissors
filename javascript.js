e2b61d9feb1517fc8ca9080cb8b8cfef99333887
dennisthekin <denniskin@protonmail.com>
2023-07-19 06:39:10 +0300

    Break down and understand the getComputerChoice function

---
+++ javascript.js
@@ -0,0 +1,4 @@
+// create a function getComputerChoice
+// create an items variable to store an array that contains 3 choices (Rock, Paper, Scissors)
+// create an randomItem variable to get and store a random choice from items array (either Rock, Paper or Scissors)
+// return randomItem
